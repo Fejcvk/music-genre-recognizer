92debad0433cc12514c87ef9df967afe981a513a
Fejcvk <tomaszfejcak@icloud.com>
2021-05-05 00:26:01 +0200

    WIP: Added SVM with gridsearch of parameters

--- README.md
+++ README.md
@@ -3,8 +3,10 @@
 ## TODO
 * Create processor class which returns preprocessed datastet [X]
 * Write a function for every algorithm in order to pass dataset and return data to be visualized [X]
+* Check accuracy for every class separately
 * Add new classification methods
     * RNN []
     * SVN []
     * Random Forest []
     * Modificaiton like CNN []
+
--- predictor.py
+++ predictor.py
@@ -1,13 +1,39 @@
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.model_selection import train_test_split, cross_validate
 from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
 from tensorflow import keras
 from tensorflow.keras import layers
 import matplotlib.pyplot as plt
 from sklearn import svm
-
+import seaborn as sns
+import random
 from dataset.dataprovider import DataProvider
+from sklearn.model_selection import GridSearchCV
+
+
+def seed_all(SEED: int):
+    random.seed(SEED)
+    np.random.seed(SEED)
+    tf.compat.v1.set_random_seed(SEED)
+
+
+def draw_distribution_plot(labels):
+    plt.figure()
+    labels.value_counts().plot.barh()
+    plt.show()
+
+
+def draw_plot(title_dict: {}, is_train:bool, x_label:str, y_label: str, data):
+    phase = 'Train' if is_train is True else 'Validation'
+    plt.title(f"{phase} accuracy for {title_dict['count']} data for {title_dict['model_type']}")
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.plot(data)
+    plt.show()
 
 
 class Predictor:
@@ -17,26 +43,29 @@ class Predictor:
         self.Y = None
         self.model = None
         self.history = None
+        self.print = False
 
     def process_data(self):
         dataset = self.dataset
-        print(dataset.head)
-        print(dataset.shape)
+        if self.print:
+            print(dataset.head)
+            print(dataset.shape)
 
         # Encode the labels
         self.Y = pd.get_dummies(dataset.label).to_numpy()
-        print(self.Y[10])
+        if self.print:
+            print(self.Y[10])
 
         # Scaling the data
         scaler = StandardScaler()
         self.X = scaler.fit_transform(np.array(dataset.iloc[:, :-1], dtype=float))
-        print(self.X[10])
+        if self.print:
+            print(self.X[10])
 
-    def train_ffnn(self, epochs: int, x_train: np.ndarray, y_train: np.ndarray, batch_count: int = 200,
+    def train_ffnn(self, epochs: int, x_train: np.ndarray, y_train: np.ndarray, batch_size: int = 64,
                    dropout: bool = False, x_test=None, y_test=None):
         print(x_train.shape)
         print(y_train.shape)
-        batch_size = int(x_train.shape[0] / batch_count)
 
         # Simple FFNN
         # 3 hidden layers with relu activation + output layer with softmax activation
@@ -58,43 +87,32 @@ class Predictor:
                                 validation_data=(x_test, y_test))
         self.model = ffnn
 
-    def train_svm(self, gamma: float, C: float, x_train: np.ndarray, y_train: np.ndarray):
-        model = svm.SVC(gamma=gamma, C=C)
-        y_train = self.flatten_labeles_vector(y_train)
+    def train_svm(self, gamma: float, C: float, x_train: np.ndarray, y_train: np.ndarray, random_state:int, kernel:str):
+        model = svm.SVC(C=C,gamma=gamma, kernel=kernel, random_state=random_state)
         model.fit(x_train, y_train)
+        # scores = cross_validate(model,x_train,y_train,scoring='f1_macro',cv=10)
+        # print(scores)
         self.model = model
 
     def predict(self, X, Y):
         return self.model.evaluate(X, Y)
 
-    def flatten_labeles_vector(self, labels_vector:np.ndarray) -> np.ndarray:
-        new_labels = np.zeros((labels_vector.shape[0],1))
-
+    def grid_search_for_svm(self, x_train, y_train):
+        param_grid = {'C': [0.1, 1, 10, 100], 'gamma': [1, 0.1, 0.01, 0.001], 'kernel': ['rbf', 'poly', 'sigmoid']}
+        grid = GridSearchCV(SVC(), param_grid, refit=True, verbose=2)
+        grid.fit(x_train, y_train)
+        print(grid.best_estimator_)
+        grid_predictions = grid.predict(X_test)
+        print(confusion_matrix(y_true=Y_test, y_pred=grid_predictions))
+        print(classification_report(Y_test, grid_predictions))
+        self.model = grid
 
     def visualize(self, model_name: str):
-        plt.title(f"Train accuracy for {self.dataset.shape[0]} data for {model_name}")
-        plt.xlabel("Iterations")
-        plt.ylabel("Accuracy")
-        plt.plot(self.history.history['accuracy'])
-        plt.show()
-
-        plt.title(f"Train loss for {self.dataset.shape[0]} data for {model_name}")
-        plt.xlabel("Iterations")
-        plt.ylabel("Loss")
-        plt.plot(self.history.history['loss'])
-        plt.show()
-
-        plt.title(f"Validation Accuracy for {self.dataset.shape[0]} data for {model_name}")
-        plt.xlabel("Iterations")
-        plt.ylabel("Accuracy")
-        plt.plot(self.history.history['val_accuracy'])
-        plt.show()
-
-        plt.title(f"Validation loss for {self.dataset.shape[0]} data for {model_name}")
-        plt.xlabel("Iterations")
-        plt.ylabel("Loss")
-        plt.plot(self.history.history['val_loss'])
-        plt.show()
+        title_dict = {'count':self.dataset.shape[0], 'model_type':model_name}
+        x_label = 'Iterations'
+        for metric in ['accuracy', 'loss']:
+            draw_plot(title_dict, is_train=True, x_label=x_label, y_label=metric, data=self.history.history[metric])
+            draw_plot(title_dict, is_train=False, x_label=x_label, y_label=metric,data=self.history.history['val_'+ metric])
 
 
 dataset_provider = DataProvider(
@@ -106,9 +124,22 @@ dataset_provider = DataProvider(
     splitted_song_duration_in_ms=3000
 )
 
+SEED = 51
+seed_all(SEED=SEED)
+
 predictor = Predictor(dataset=dataset_provider.get_or_create_dataset())
 predictor.process_data()
-X_train, X_test, Y_train, Y_test = train_test_split(predictor.X, predictor.Y, test_size=.2)
+
+# X_train, X_test, Y_train, Y_test = train_test_split(predictor.X, predictor.Y, test_size=.2)
 # predictor.train_ffnn(epochs=300, x_train=X_train, y_train=Y_train, dropout=True, x_test=X_test, y_test=Y_test)
 # predictor.visualize(model_name='FFNN')
-predictor.train_svm(gamma=0.0001, C=100., x_train=X_train, y_train=Y_train)
+# print(predictor.model.evaluate(X_test, Y_test))
+
+# draw_distribution_plot(Y_train)
+# draw_distribution_plot(Y_test)
+
+#Optimal hyperparameters C=10 gamma=0.1 found by gridsearch
+X_train, X_test, Y_train, Y_test = train_test_split(predictor.X, predictor.dataset.label, test_size=.2)
+predictor.train_svm(gamma=0.1,C=10, kernel="rbf",random_state=SEED,x_train=X_train,y_train=Y_train)
+print(f"SVN accuracy = {predictor.model.score(X_test, Y_test)}")
+print(classification_report(y_pred=predictor.model.predict(X_test), y_true=Y_test))
